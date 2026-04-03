32e77833ad0f36d76fe8301f712df2bb60f37f34
TechHog <techhog@example.com>
2026-04-03 19:49:08 -0400

    structure cleanup

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-cp jsinjection.js jsinjection_build.js || exit 1
+cp src/jsinjection.js jsinjection_build.js || exit 1
 
 sed -i '/LUAU_CODE_HERE/{
-  r ./Luau.Web.js
+  r dependencies/Luau.Web.js
   d
 }' jsinjection_build.js || exit 1
